ae4edddcb7a95272a4aa0841075e2a667f426c8e
Rohit Kumar <kumar1rohit@outlook.com>
2024-08-26 22:10:34 +0530

    Add video thumbnail and update home page

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,31 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import videoThumbnail from '../assets/video-thumbnail.jpg'
 
 function Home() {
     return (
         <div className="w-full max-w-2xl mx-auto p-6 bg-shareClaude-background rounded-lg text-center">
-            <h2 className="text-xl sm:text-2xl font-semibold mb-6 text-gray-200">
-                <span className="block">Chrome Extension to share</span>
-                <span className="block">your Claude chats</span>
-                <span className="block">with one click</span>
-            </h2>
-            <div className="flex flex-col sm:flex-row justify-center gap-4 items-center">
+            <p className="text-xl sm:text-2xl font-semibold mb-6 text-gray-200">
+                Chrome Extension to share Claude chats with one click
+            </p>
+            <div className="mb-6 flex justify-center gap-4 items-center">
                 <Link
-                    to="https://github.com/rohit1kumar/shareclaude?tab=readme-ov-file#how-to-use"
+                    to="https://github.com/rohit1kumar/shareclaude?tab=readme-ov-file#shareclaude"
                     target="_blank"
+                    rel="noopener noreferrer"
                     className="w-auto max-w-xs px-4 py-2 bg-shareClaude-userChat text-white rounded-lg hover:bg-shareClaude-accent/80 whitespace-nowrap"
                 >
-                    how to use
+                    How to Use
                 </Link>
 
                 <Link
                     to="/c/rhxw367ndulkfr24a5hssm5u"
                     className="w-auto max-w-xs px-4 py-2 bg-shareClaude-claudeChat text-white rounded-lg hover:bg-shareClaude-accent/80 whitespace-nowrap"
                 >
-                    view a chat
+                    View a Chat
                 </Link>
             </div>
-        </div>
+            <div className="aspect-video overflow-hidden rounded-lg border border-gray-600 shadow-lg shadow-shareClaude-accent/30 hover:shadow-shareClaude-accent/50 transition-all duration-300">
+                <video
+                    controls
+                    disablepictureinpicture
+                    controlsList="nodownload noplaybackrate noremoteplayback"
+                    src="https://i.imgur.com/NRAM7Lf.mp4"
+                    className="w-full h-full"
+                    type="video/mp4"
+                    poster={videoThumbnail}
+                >
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div >
     )
 }
 
