11a97b06afb816b0d04425fa44e5249f23ca6fef
Rohit Kumar <kumar1rohit@outlook.com>
2024-08-27 12:43:34 +0530

    Update Home page layout and add download link for Chrome extension

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -10,32 +10,28 @@ function Home() {
             <p className="text-lg sm:text-xl font-semibold mb-6 text-gray-200">
                 Chrome Extension to Share Claude Chats with One Click
             </p>
-            <div className="mb-6 flex justify-center gap-4 items-center">
+            <div className="mb-6 flex flex-col sm:flex-row justify-center gap-4 items-center">
                 <Link
-                    to="https://github.com/rohit1kumar/shareclaude?tab=readme-ov-file#shareclaude"
+                    to="https://github.com/rohit1kumar/shareclaude"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="w-auto max-w-xs px-4 py-2 bg-shareClaude-userChat text-white rounded-lg hover:bg-shareClaude-accent/80 whitespace-nowrap"
-                >
-                    How to Use
+                    className="w-full sm:w-auto px-4 py-2 bg-shareClaude-userChat text-white rounded-lg hover:bg-shareClaude-accent/80">
+                    Download Extension
                 </Link>
-
                 <Link
                     to="/c/rhxw367ndulkfr24a5hssm5u"
-                    className="w-auto max-w-xs px-4 py-2 bg-shareClaude-claudeChat text-white rounded-lg hover:bg-shareClaude-accent/80 whitespace-nowrap"
-                >
-                    View a Chat
+                    className="w-full sm:w-auto px-4 py-2 bg-shareClaude-claudeChat text-white rounded-lg hover:bg-shareClaude-accent/80">
+                    View an Example Chat
                 </Link>
             </div>
             <div className="aspect-video overflow-hidden rounded-xl border border-gray-600 shadow-lg shadow-shareClaude-accent/50 hover:shadow-shareClaude-accent/80 transition-all duration-300">
                 <LiteYouTubeEmbed
                     id="OHkNa85A_fg"
-                    // poster="default"
                     thumbnail={thumbnail}
                     title="ShareClaude - Chrome Extension for Sharing Claude Conversations"
                 />
             </div>
-        </div >
+        </div>
     )
 }
 
