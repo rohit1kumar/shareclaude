27293420567698fe7f371fdc500ece66acc26c1c
Rohit Kumar <kumar1rohit@outlook.com>
2024-11-05 21:11:30 +0530

    add posthog for anayltics

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.jsx'
 import './index.css'
 
+const options = {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
