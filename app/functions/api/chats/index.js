f86aebab74bf5130c4df43f507f7d9a7aac0050d
Rohit Kumar <kumar1rohit@outlook.com>
2024-11-28 00:52:06 +0530

    Update index.js

--- app/functions/api/chats/index.js
+++ app/functions/api/chats/index.js
@@ -9,15 +9,15 @@ export async function onRequestPost(context) {
     const db = drizzle(context.env.DB);
     try {
         const { title, content } = await context.request.json();
+        console.log({ title, content })
         if (!title.trim()) {
             return Response.json({ msg: 'title is required' }, { status: 400 });
         }
-        const [newChat] = await db.insert(chatsSchema).values({ title, content }).returning()
         return Response.json({
             id: newChat.id,
         }, { status: 201 });
     } catch (error) {
-        console.log("Error creating chat: ", error)
+        // console.log("Error creating chat: ", error)
         return Response.json({ msg: "something went wrong!" }, { status: 500 });
     }
 }
